0aee8f077335c22d80744fb9070117110191ade3
Bruno-ferrariv <bunofvincensi@gmail.com>
2021-09-23 20:02:55 -0300

    alteracao

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const dummyTransactions = [
 
 {id : 1, name : 'Bolo ', amount: -20},
 {id : 2, name : 'Salario', amount: -30},
-{id : 3, name : 'Torta ', amount: -20},
-{id : 4, name : 'violao ', amount: 150},
+{id : 3, name : 'Torta', amount: -20},
+{id : 4, name : 'violao', amount: 150},
 
 ]
 
@@ -19,7 +19,7 @@ const addTransactionIntoDom = transaction =>{
   li.classList.add(CSSClass)
 
    li.innerHTML = `
-        ${transaction.name}<span> ${operator} R$ ${amountWihoutOperator} </span>
+        ${transaction.name}<span> ${operator} R$ ${amountWithoutOperator} </span>
                                 <button class="delete-btn">x</button> `
    transactionsUl.append(li);                  
 
@@ -29,9 +29,31 @@ const addTransactionIntoDom = transaction =>{
 /*Metodo da soma*/
 
 const updateBalanceValues = () =>{
+//pegar todos os valores do objeto
+    const transactionAmounts = dummyTransactions.map(transaction => transaction.amount);
 
-    const transactionAmonts = dummyTransactions.map(transaction => transaction.amount);
-    console.log(transactionAmonts);
+
+
+      // somatorio dos valores
+      const total = transactionAmounts.reduce((acumulator, transaction) => acumulator + transaction, 0)
+      .toFixed(2);
+
+
+    console.log(transactionAmounts);
+    console.log("valor do reduce " + total);
+
+
+    //RETORNA AS RECEITAS
+    const income = transactionAmounts
+    .filter(values => values > 0)
+    .reduce((acumulator, values) => acumulator + values, 0)
+    .toFixed(2);
+
+    //RETORNA AS DESPESAS
+    const expense = transactionAmounts
+    .filter(values => values > 0)
+    .reduce((acumulator, values) => acumulator - values, 0)
+    .toFixed(2);
 }
 
 
