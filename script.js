1cec963790d0bae38bb8b7f14b398b786f3c4c2f
Bruno-ferrariv <bunofvincensi@gmail.com>
2021-09-24 20:27:26 -0300

    implementando códigos para gerar o funcionamento do programa

--- script.js
+++ script.js
@@ -1,74 +1,88 @@
-const transactionsUl = document.querySelector('#transaction');
+// pegar componente por ID
+const transactionsUl = document.querySelector('#transactions');
+const moneyPlusDisplay = document.querySelector('#money-plus');
+const moneyMinusDisplay = document.querySelector('#money-minus');
+const balanceDisplay = document.querySelector('#balance');
+const inputTransactionName = document.querySelector('#text');
+const inputTransactionAmount = document.querySelector('#amount');
 
+// declaraccao de um objeto literal
 const dummyTransactions = [
-
-{id : 1, name : 'Bolo ', amount: -20},
-{id : 2, name : 'Salario', amount: -30},
-{id : 3, name : 'Torta', amount: -20},
-{id : 4, name : 'violao', amount: 150},
-
+    
 ]
-
-const addTransactionIntoDom = transaction =>{
-
-  const operator = transaction.amount < 0 ? '-' : '+';
-  const amountWithoutOperator = Math.abs(transaction.amount);
-  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
-  const li = document.createElement('li');
-
-  li.classList.add(CSSClass)
-
-   li.innerHTML = `
-        ${transaction.name}<span> ${operator} R$ ${amountWithoutOperator} </span>
-                                <button class="delete-btn">x</button> `
-   transactionsUl.append(li);                  
-
-
+const addTransactionIntoDom = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'; // if ternario ou if linha
+    const amountWithoutOperator = Math.abs(transaction.amount);
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+    const li = document.createElement('li');
+    li.classList.add(CSSClass);
+    li.innerHTML = ` 
+            ${transaction.name} <span> ${operator} R$  ${amountWithoutOperator} </span> 
+                                    <button class="delete-btn">x</button> `
+    transactionsUl.append(li);
 }
 
-/*Metodo da soma*/
+/*
+    Metodo que ira retornar todo os valores somados de receitas e despesas
+    */
+const updateBalanceValues = () => {
 
-const updateBalanceValues = () =>{
-//pegar todos os valores do objeto
+    // pegar todos os valores do objeto dummy
     const transactionAmounts = dummyTransactions.map(transaction => transaction.amount);
 
+    // somatorio dos valores retornados    
+    const total = transactionAmounts
+        .reduce((acumulator, transaction) => acumulator + transaction, 0)
+        .toFixed(2);
 
-
-      // somatorio dos valores
-      const total = transactionAmounts.reduce((acumulator, transaction) => acumulator + transaction, 0)
-      .toFixed(2);
-
-
-    console.log(transactionAmounts);
-    console.log("valor do reduce " + total);
-
-
-    //RETORNA AS RECEITAS
+    // retorna todas as receitas
     const income = transactionAmounts
-    .filter(values => values > 0)
-    .reduce((acumulator, values) => acumulator + values, 0)
-    .toFixed(2);
+        .filter(values => values > 0)
+        .reduce((acumulator, values) => acumulator + values, 0)
+        .toFixed(2);
 
-    //RETORNA AS DESPESAS
+    // retorna todas as despesas
     const expense = transactionAmounts
-    .filter(values => values > 0)
-    .reduce((acumulator, values) => acumulator - values, 0)
-    .toFixed(2);
+        .filter(values => values < 0)
+        .reduce((acumulator, values) => acumulator - values, 0)
+        .toFixed(2);
+
+    balanceDisplay.textContent = `R$ ${total}`
+    moneyPlusDisplay.textContent = `R$ ${income}`
+    moneyMinusDisplay.textContent = `R$ ${expense}`
+
 }
-
-
-
-
-
-
-const init = () =>{
-dummyTransactions.forEach(addTransactionIntoDom)
-updateBalanceValues();
-
-
+const init = () => { // inicializa todas as funções da pagina
+    transactionsUl.innerHTML = '';
+    dummyTransactions.forEach(addTransactionIntoDom);
+    updateBalanceValues();
 }
 
 init();
-//<li class="plus">
-// Salario <span>$400</span><button class="delete-btn">x</button>
-// </li>
+//  funcao que gera ID
+const generateId = () => Math.round(Math.random() * 1000);
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const transactionName = inputTransactionName.value.trim();
+    const transactionAmout = inputTransactionAmount.value.trim();
+
+    if (transactionName === "" && transactionAmout === "") {
+        alert('Informe os campos valor e nome da transação');
+        return
+    }
+
+    const transaction = {
+        id: generateId(),
+        name: transactionName,
+        amount: Number(transactionAmout)
+    }
+
+    dummyTransactions.push(transaction);
+    init();
+    updateLocalStorage;
+
+    transactionName.value = '';
+    transactionAmout.value = '';
+});
